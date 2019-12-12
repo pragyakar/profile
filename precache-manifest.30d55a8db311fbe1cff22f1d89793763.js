2e1ee4510e7f9fe186412a9a9b1d0e25f437d6b4
pragyakar <pragyakar75@gmail.com>
2019-12-12 17:07:24 +0545

    Updates

---
+++ precache-manifest.30d55a8db311fbe1cff22f1d89793763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ee42fd8766ab24e531dc5e0c6285eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80cc695c642d34eb60ef",
+    "url": "/static/css/main.f63289ae.chunk.css"
+  },
+  {
+    "revision": "b5a95292f2bd47d3ca3a",
+    "url": "/static/js/2.1a87f580.chunk.js"
+  },
+  {
+    "revision": "80cc695c642d34eb60ef",
+    "url": "/static/js/main.cbea248c.chunk.js"
+  },
+  {
+    "revision": "f4961853731587411123",
+    "url": "/static/js/runtime-main.cc89a185.js"
+  }
+]);
