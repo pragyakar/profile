81610e8b71a895bc22f0c8e284d5427dec6d7bed
pragyakar <pragyakar75@gmail.com>
2019-12-12 16:21:52 +0545

    :pencil: Updated medium profile link

--- src/components/Landing/Links/Links.jsx
+++ src/components/Landing/Links/Links.jsx
@@ -7,7 +7,7 @@ import * as fab from '@fortawesome/free-brands-svg-icons';
 const Links = () => (
   <div className="icons-box">
     <Icon name={'GitHub'} link={'https://github.com/pragyakar'} icon={fab.faGithub} />
-    <Icon name={'Read my Blog'} link={''} icon={fab.faMedium} />
+    <Icon name={'Read my Blog'} link={'https://medium.com/@pragyakar'} icon={fab.faMedium} />
     <Icon name={'LinkedIn'} link={'https://www.linkedin.com/in/pragyakar/'} icon={fab.faLinkedin} />
     <Icon name={'Write to me'} link={'mailto:pragyakarj@gmail.com'} icon={fa.faMailBulk} />
     <Icon name={'Download CV'} link={''} icon={fa.faFile} />
