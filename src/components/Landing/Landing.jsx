d065867ffb5ef865db6dde0c39c20efc465159a3
pragyakar <pragyakar75@gmail.com>
2019-12-13 15:57:04 +0545

    :sparkles:  Added fading animation

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Links from './Links';
 
-const Landing = () => (
-  <div className="landing">
-    <div className="content-box">
-      <div className="title-box">
-        <span>pragyakar</span>
-      </div>
-      <Links />
-    </div>
-  </div>
-)
+const Landing = () => {
+
+  const [loadingState, setLoadingState] = useState(false);
+  
+  useEffect(() => ( 
+    setLoadingState(true)
+  ), []);
+
+  return (
+    <React.Fragment>
+      <div className="landing">
+        <div className={ `content-box ${ loadingState ? 'visible' : ''}`}>
+          <div className="title-box">
+            <span>pragyakar</span>
+          </div>
+          <Links />
+        </div>
+      </div> 
+    </React.Fragment>
+  )
+}
 
 export default Landing;
