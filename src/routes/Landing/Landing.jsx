b496d23a014ac47a17660d90339985aed7114e69
pragyakar <pragyakar75@gmail.com>
2021-05-26 12:59:05 +0545

    :sparkles: Add global css values

--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Links from "./Links";
 
 const Landing = () => {
   return (
     <div className="pr-landing">
       <span className="pr-landing__title">Hi, I'm Pragyakar!</span>
+      <Links />
     </div>
   );
 };
--- src/routes/Landing/Links.jsx
+++ src/routes/Landing/Links.jsx
@@ -5,9 +5,9 @@ const Links = () => {
   return (
     <div className="pr-links">
       <GitHub className="pr-links__icon" />
-      <Linkedin className="pr-links__icon" />
-      <Paperclip className="pr-links__icon" />
-      <Send className="pr-links__icon" />
+      <Linkedin className="pr-links__icon"/>
+      <Paperclip className="pr-links__icon"/>
+      <Send className="pr-links__icon"/>
     </div>
   );
 };
