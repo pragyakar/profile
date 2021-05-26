ca268bf80be05308b2a667c0aba5cf33114a58c3
pragyakar <pragyakar75@gmail.com>
2021-05-26 14:53:27 +0545

    :sparkles: Add cutout components

--- src/components/Cutouts/TopRippleCutout.jsx
+++ src/components/Cutouts/TopRippleCutout.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const TopRippleCutout = () => {
   return (
     <>
+      <div className="top-ripple-cutout ripple-macha ripple-cutout-xs" />
       <div className="top-ripple-cutout ripple-macha ripple-cutout-sm" />
       <div className="top-ripple-cutout ripple-macha ripple-cutout-md" />
       <div className="top-ripple-cutout ripple-macha ripple-cutout-lg" />
