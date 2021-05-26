70ef2eae0c1a929bf895fb571ccb8f7a52ea5b2f
pragyakar <pragyakar75@gmail.com>
2021-05-26 14:12:06 +0545

    :sparkles: Add cutout components

---
+++ src/components/Cutouts/TopRippleCutout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopRippleCutout = () => {
+  return (
+    <>
+      <div className="top-ripple-cutout ripple-macha ripple-cutout-sm" />
+      <div className="top-ripple-cutout ripple-macha ripple-cutout-md" />
+      <div className="top-ripple-cutout ripple-macha ripple-cutout-lg" />
+    </>
+  );
+};
+
+export default TopRippleCutout;
--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Links from "./Links";
+import TopRippleCutout from "../../components/Cutouts/TopRippleCutout";
+import BottomRippleCutout from "../../components/Cutouts/BottomRippleCutout";
 
 const Landing = () => {
   return (
     <div className="pr-landing">
       <span className="pr-landing__title">Hi, I'm Pragyakar!</span>
       <Links />
+      <TopRippleCutout />
+      <BottomRippleCutout />
     </div>
   );
 };
